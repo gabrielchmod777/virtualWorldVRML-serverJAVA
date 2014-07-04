b94cc469c4c8e4d33a826a5bf4dba005dc672cd6
Gabriel Muresan <gabriel.mcx@gmail.com>
2014-07-04 23:58:09 +0300

    ...

--- L3DServer/library.js
+++ L3DServer/library.js
@@ -1,19 +1,20 @@
-function avatar(name) {
+function avatar(name, type) {
 	this.name = name;
+	this.type = type;
 
 	this.talk = function(name, chatID ,text) {
-	var toSend = "<client_plugin>./plugins/libAvatar.so</client_plugin><client_plugin_source>libAvatar.so</client_plugin_source><action>talk</action><other_user>"+name+"</other_user><chat_id>"+chatID+"</chat_id><say>"+text+"</say>";
+	var toSend = "<client_plugin>./plugins/libAvatar.so</client_plugin><client_plugin_source>libAvatar.so</client_plugin_source><action>talk</action><other_user>"+name+"</other_user><avatar_type_id>"+this.type+"</avatar_type_id><chat_id>"+chatID+"</chat_id><say>"+text+"</say>";
 	return toSend;
 	
 	}
 	
 	this.move = function(mx, my,  mz) {
-		var toSend = "<client_plugin>libAvatar.so</client_plugin><client_plugin_source>http://localhost/plugins/libAvatar.so</client_plugin_source><action>move</action><other_user>"+name+"</other_user><x>"+mx+"</x><y>"+my+"</y><z>"+mz+"</z>";
+		var toSend = "<client_plugin>libAvatar.so</client_plugin><client_plugin_source>http://localhost/plugins/libAvatar.so</client_plugin_source><action>move</action><other_user>"+name+"</other_user><avatar_type_id>"+this.type+"</avatar_type_id><x>"+mx+"</x><y>"+my+"</y><z>"+mz+"</z>";
 		return toSend;
 	}
 
 	this.rotate = function(axis, degrees) {
-		var toSend = "<client_plugin>libAvatar.so</client_plugin><client_plugin_source>http://localhost/plugins/libAvatar.so</client_plugin_source><action>rotate</action><other_user>"+name+"</other_user><axis>"+axis+"</axis><degrees>"+degrees+"</degrees>";
+		var toSend = "<client_plugin>libAvatar.so</client_plugin><client_plugin_source>http://localhost/plugins/libAvatar.so</client_plugin_source><action>rotate</action><other_user>"+name+"</other_user><avatar_type_id>"+this.type+"</avatar_type_id><axis>"+axis+"</axis><degrees>"+degrees+"</degrees>";
 		return toSend;
 	}
 	
